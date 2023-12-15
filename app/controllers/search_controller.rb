ec6b58404e06dfd1753e037636ddb03d880854e0
Scott DeVoss <scottdevoss@yahoo.com>
2023-12-15 09:28:17 -0700

    added faraday connection response

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
-    
+    nation = params[:nation]
+    response = Faraday.get("https://last-airbender-api.fly.dev/api/v1/characters?affiliation=#{nation}")
+    @data = JSON.parse(response.body, symbolize_names: true)
   end
 end
